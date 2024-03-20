e15e0301a604061e68734c6cdaf686029cc14eac
askp199 <askp199@gmail.com>
2024-03-19 23:40:40 -0500

    updated report()

--- main.py
+++ main.py
@@ -2,17 +2,20 @@
 
 def main():
 
+    #sets path to file and reads it in
+    book = 'Frankenstein'
     path_to_file = "books/frankenstein.txt"
 
     with open(path_to_file) as f:
         file_contents = f.read()
 
+    #Counts the words in a book
     def word_count():
         words = file_contents.split()
         count = len(words)
         return count
 
-
+    # Counts the letters in the book
     def letter_count():
         lower_case_file = file_contents.lower()
         letter_count_dict = {}
@@ -27,22 +30,44 @@ def main():
         
         return letter_count_dict
     
+    #Generates a report including the word count and the letter count of the book
     def report():
         word = word_count()
         letter = letter_count()
         list_dict = []
-        for i, val in letter.items():
+        
+        #sorts the letters in alphabetical order
+        my_keys = list(letter.keys())
+        my_keys.sort()
+        sorted_dict = {i: letter[i] for i in my_keys}
+        print(sorted_dict)
+
+        #creates a new list with only alphabet characters
+        for i, val in sorted_dict.items():
             key = f"{i}"
             if i.isalpha():
                 new_dict = {}
                 new_dict[key]=val
                 list_dict.append(new_dict)
-        
-        print(list_dict)
+
+        #Prints the text of the report
+        print(f"------Begin report on {book}-----")
+        print("")
+        print(f"The word count is {word}.")
+        print(f"")
+
+        for i in range(len(list_dict)):
+
+            dictonary = list_dict[i]
+            for keys, value in dictonary.items():
+                new_string = f"The letter {keys} was found {value} times."
+                print(new_string)
+
+        print("")
+        print(f"-----End Report-----")
+
     
     report()
-
-
 
 
 if __name__ == '__main__':
